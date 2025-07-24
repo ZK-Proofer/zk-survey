998a6a5e4e56a2e5d8753f134e2466b41f4fc25c
박태석 <qkrxotjr9508@gmail.com>
2025-07-24 21:49:04 +0900

    chore : frontend ci/cd

--- packages/frontend/ecosystem.config.js
+++ packages/frontend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "zk-survey-frontend",
-      script: "yarn start",
+      script: "yarn start -p 9112",
       instances: "1",
       exec_mode: "fork",
     },
